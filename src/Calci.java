a15269d22406c034d7db111a0cde97b6caaf50fa
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:05:21 +0530

    commited

--- src/Calci.java
+++ src/Calci.java
@@ -2,8 +2,16 @@
 public class Calci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Addition a=new Addition();
+		Subtraction s =new Subtraction();
+		Multiplication m=new Multiplication();
+		Division d=new Division();
+		a.add(10,20);
+		s.sub(20,10);
+		m.mul(10, 20);
+		d.div(10,10);
+		
+		
 	}
 
 }
